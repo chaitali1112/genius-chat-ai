2b76a7678e86a1392df0331da815142efb95a81a
chaitali1112 <chaitalimali42@gmail.com>
2026-04-27 19:38:03 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -51,7 +51,7 @@ function ChatPage() {
     setIsLoading(true);
 
     try {
-      const res = await fetch(WEBHOOK_URL, {
+      const res = await fetch(https://taskflow.app.n8n.cloud/webhook/mychatapp, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
